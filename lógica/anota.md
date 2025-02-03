6c2ec9f62466d53876311046e2e017bd3f2e2b41
Daiana Leite <daianaleite11@gmail.com>
2025-02-03 14:36:15 -0300

    2

---
+++ lógica/anota.md
@@ -0,0 +1,9 @@
+# estrutura de dados
+
+## list
+
+- lista: ordenada e mutável
+- homogêneo: todos os elementos da lista tem o mesmo tipo
+- heterogêneo: todos os elementos da lista podem ter tipos diferentes
+- criação de lista-> os itens são separados por virgula
+- termina com ()-> é um método
--- lógica/aula08/listas.py
+++ lógica/aula08/listas.py
@@ -1,13 +1,8 @@
-'''
-- lista: ordenada e mutável
--->homogêneo: todos os elementos da lista tem o mesmo tipo
--->heterogêneo: todos os elementos da lista podem ter tipos diferentes
-criação de lista-> os itens são separados por virgula
-termina com ()-> é um método
-'''
 
 sabores = ["mussarela", "calabresa","frango com catupiry", "portuguesa"]
 daddos_pizza = ['mussarela', 26.9]
+daddos_pizza2 = ['mussarela', 26.9, sabores]
 
 print("Os sabores disponiveis sao: ", sabores)
 print("InformaçÕes sobre os sabores da pizza: ", daddos_pizza[0], "que custa ", daddos_pizza[1])
+print(daddos_pizza2)
